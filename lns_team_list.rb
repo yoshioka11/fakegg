34082095f917f183dff71b547e27c18ac6a3e05e
yoshioka11 <yuuki.yoshioka11@gmail.com>
2017-10-31 00:09:15 +0900

    チーム情報とURL取得

---
+++ lns_team_list.rb
@@ -0,0 +1,15 @@
+require 'mechanize'
+
+agent = Mechanize.new
+
+page = agent.get('http://lns-lol.com/Ladder/display')
+elements = page.search('tr a')
+
+teams = {}
+
+elements.each do |ele|
+  teams["#{ele.text}"] = ele[:href]
+end
+
+puts teams
+
